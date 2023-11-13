95df1e340be464f2f07a74efcd3afa781b755649
Mateo <mateopoey@gmail.com>
2023-11-13 18:25:10 -0300

    Crosshair

---
+++ TankvsPlanes/Assets/Scripts/TankScripts/Crosshair.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    public GameObject crosshairPrefab;
+
+
+    private GameObject crosshairInstance;
+
+    private void Start()
+    {
+        if (crosshairPrefab == null)
+        {
+            Debug.LogError("Crosshair prefab not assigned!");
+            return;
+        }
+
+      
+        crosshairInstance = Instantiate(crosshairPrefab);
+    }
+
+    private void Update()
+    {
+        
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 10f; 
+        crosshairInstance.transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+}
