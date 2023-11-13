134f2b59d67eff019cc1c51b030e5eeac9b9d6ed
Mateo <mateopoey@gmail.com>
2023-11-13 17:19:17 -0300

    Limite de Apuntado de Cañon
    El cañon no apunta para abajo

--- TankvsPlanes/Assets/Scripts/TankScripts/CanonController.cs
+++ TankvsPlanes/Assets/Scripts/TankScripts/CanonController.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CanonController : MonoBehaviour
@@ -10,10 +9,11 @@ public class CanonController : MonoBehaviour
     public float bulletSpeed = 10f;
     public float timeBtwShots = 1;
     private float timeOfLastShot;
+    private float minRotation = -90f;
 
     void Start()
     {
-        // Esconder Cursor
+
         Cursor.visible = false;
     }
 
@@ -29,21 +29,28 @@ public class CanonController : MonoBehaviour
 
         Vector3 direction = (mousePosition - transform.position).normalized;
 
-        Quaternion rotation = Quaternion.LookRotation(Vector3.forward, direction);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        angle -= 90f;
+
+        angle = Mathf.Clamp(angle, minRotation, 90f);
+
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
         transform.rotation = rotation;
     }
+
 
     void Shoot()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            if (Time.time - timeOfLastShot >= timeBtwShots) //Cooldown de tiro
+            if (Time.time - timeOfLastShot >= timeBtwShots) // Cooldown de tiro
             {
                 GameObject bullet = Instantiate(bulletPrefab, firePoint.position, transform.rotation);
-            Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
-            bulletRb.AddForce(transform.up * bulletSpeed, ForceMode2D.Impulse);
-            timeOfLastShot = Time.time;
+                Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+                bulletRb.AddForce(transform.up * bulletSpeed, ForceMode2D.Impulse);
+                timeOfLastShot = Time.time;
             }
         }
     }
