d34eb51d6423c20eb1a19f139f4b00b0a4151a75
Nechi025 <ignacioaloswol@hotmail.com>
2023-11-23 12:36:12 -0300

    Menu con sonido

--- TankvsPlanes/Assets/Scripts/MainMenu/MainMenu.cs
+++ TankvsPlanes/Assets/Scripts/MainMenu/MainMenu.cs
@@ -5,12 +5,66 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Update is called once per frame
+    public GameObject mainMenu;
+    public GameObject selectMenu;
+    public GameObject selected;
+    private int index = 1;
+
+    [SerializeField] private AudioClip switchSelect;
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
-            SceneManager.LoadScene(1);
+            if (gameObject.activeSelf == true)
+            {
+                mainMenu.SetActive(false);
+                selectMenu.SetActive(true);
+            }
+
+            if (selectMenu.activeSelf == true && index == 0)
+            {
+                SceneManager.LoadScene(1);
+            }
+
+            if (selectMenu.activeSelf == true && index == 5)
+            {
+                Application.Quit();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.S) && index < 5 && selectMenu.activeSelf == true)
+        {
+            index++;
+            SoundManager.Instance.playSound(switchSelect);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && index > 0 && selectMenu.activeSelf == true)
+        {
+            index--;
+            SoundManager.Instance.playSound(switchSelect);
+        }
+
+        switch (index)
+        {
+            case 0:
+                selected.transform.position = new Vector3(760, 590, 0);
+                break;
+            case 1:
+                selected.transform.position = new Vector3(760, 490, 0);
+                break;
+            case 2:
+                selected.transform.position = new Vector3(760, 390, 0);
+                break;
+            case 3:
+                selected.transform.position = new Vector3(760, 320, 0);
+                break;
+            case 4:
+                selected.transform.position = new Vector3(760, 220, 0);
+                break;
+            case 5:
+                selected.transform.position = new Vector3(760, 150, 0);
+                break;
         }
     }
 }
