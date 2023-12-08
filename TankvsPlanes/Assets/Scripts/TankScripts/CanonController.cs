d59b9896ab1d48c0f5be7c6bf119144fbf9637a2
Mateo <mateopoey@gmail.com>
2023-12-07 22:17:23 -0300

    Muchos Cambios

--- TankvsPlanes/Assets/Scripts/TankScripts/CanonController.cs
+++ TankvsPlanes/Assets/Scripts/TankScripts/CanonController.cs
@@ -3,17 +3,33 @@ using UnityEngine;
 public class CanonController : MonoBehaviour
 {
     [SerializeField] private Camera cam;
-    public GameObject bulletPrefab;
     public Transform firePoint;
+
+
+    public GameObject bulletPrefab;
     public float bulletSpeed = 10f;
     public float timeBtwShots = 1;
     private float timeOfLastShot;
-    private float minRotation = -90f;
+
+
+    
+    public GameObject altBulletPrefab;
+    public float altBulletSpeed = 10f;
+    public float altTimeBtwShots = 1;
+    private float altTimeOfLastShot;
+
 
     private GameObject tanqueCuerpo;
     private SpriteRenderer tanqueCuerpoRenderer;
 
+
+    private float minRotation = -90f;
     public float rotationSpeed = 5f;
+
+
+    private int fire1 = 0;
+    public float timeBtwChange = 3;
+    private float timeLastChange;
 
     void Start()
     {
@@ -23,13 +39,43 @@ public class CanonController : MonoBehaviour
         tanqueCuerpoRenderer = tanqueCuerpo.GetComponent<SpriteRenderer>();
     }
 
+
+
     private void Update()
     {
         Aim();
-        Shoot();
+
+        
+
+
+        if (Input.GetButton("Fire1")) 
+        {
+            if (fire1 == 0)
+            {
+                Shoot();
+            }
+            else
+            {
+                ShootAlt();
+            }
+
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            
+            if (Time.time - timeLastChange >= timeBtwChange)
+            {
+                ChangeBulletType();
+                timeLastChange = Time.time;
+            }
+
+        }
 
         GetComponent<SpriteRenderer>().color = tanqueCuerpoRenderer.color;
     }
+
+
 
     void Aim()
     {
@@ -44,17 +90,50 @@ public class CanonController : MonoBehaviour
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
     }
 
+    void ChangeBulletType()
+    {
+
+        if (fire1 == 0)
+        { 
+        fire1 = 1;
+        
+        }
+
+        else if(fire1 == 1)
+        {
+            fire1 = 0;
+           
+
+        }
+        
+    }
+
+
+
     void Shoot()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Time.time - timeOfLastShot >= timeBtwShots)
         {
-            if (Time.time - timeOfLastShot >= timeBtwShots) // Cooldown de tiro
-            {
-                GameObject bullet = Instantiate(bulletPrefab, firePoint.position, transform.rotation);
-                Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
-                bulletRb.AddForce(transform.up * bulletSpeed, ForceMode2D.Impulse);
-                timeOfLastShot = Time.time;
-            }
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, transform.rotation);
+            Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+            bulletRb.AddForce(transform.up * bulletSpeed, ForceMode2D.Impulse);
+            timeOfLastShot = Time.time;
         }
     }
+
+
+
+    void ShootAlt()
+    {
+        if (Time.time - altTimeOfLastShot >= altTimeBtwShots)
+        {
+            GameObject bullet = Instantiate(altBulletPrefab, firePoint.position, transform.rotation);
+            Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+            bulletRb.AddForce(transform.up * altBulletSpeed, ForceMode2D.Impulse);
+            altTimeOfLastShot = Time.time;
+        }
+    }
+
+
+
 }
